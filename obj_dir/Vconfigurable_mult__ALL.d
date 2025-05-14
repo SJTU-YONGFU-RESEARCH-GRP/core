027008b966eec9f2fabe6fd313d9d71a0b4a1d85
Yongfu Li <yongfu.unix@gmail.com>
2025-05-14 14:36:50 +0800

    update

---
+++ obj_dir/Vconfigurable_mult__ALL.d
@@ -0,0 +1,19 @@
+Vconfigurable_mult__ALL.o: Vconfigurable_mult__ALL.cpp \
+ Vconfigurable_mult.cpp Vconfigurable_mult__pch.h \
+ /usr/share/verilator/include/verilated.h \
+ /usr/share/verilator/include/verilatedos.h \
+ /usr/share/verilator/include/verilated_config.h \
+ /usr/share/verilator/include/verilated_types.h \
+ /usr/share/verilator/include/verilated_funcs.h \
+ Vconfigurable_mult__Syms.h Vconfigurable_mult.h \
+ Vconfigurable_mult___024root.h \
+ /usr/share/verilator/include/verilated_vcd_c.h \
+ /usr/share/verilator/include/verilated.h \
+ /usr/share/verilator/include/verilated_trace.h \
+ Vconfigurable_mult___024root__DepSet_hc4ba6f57__0.cpp \
+ Vconfigurable_mult___024root__DepSet_h14e80911__0.cpp \
+ Vconfigurable_mult__Trace__0.cpp Vconfigurable_mult___024root__Slow.cpp \
+ Vconfigurable_mult___024root__DepSet_hc4ba6f57__0__Slow.cpp \
+ Vconfigurable_mult___024root__DepSet_h14e80911__0__Slow.cpp \
+ Vconfigurable_mult__Syms.cpp Vconfigurable_mult__Trace__0__Slow.cpp \
+ Vconfigurable_mult__TraceDecls__0__Slow.cpp
---
+++ obj_dir/Vconfigurable_mult__TraceDecls__0__Slow.cpp
@@ -0,0 +1,11 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Tracing declarations
+#include "verilated_vcd_c.h"
+
+
+void Vconfigurable_mult___024root__traceDeclTypesSub0(VerilatedVcd* tracep) {
+}
+
+void Vconfigurable_mult___024root__trace_decl_types(VerilatedVcd* tracep) {
+    Vconfigurable_mult___024root__traceDeclTypesSub0(tracep);
+}
